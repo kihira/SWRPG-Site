f0c136b26100b548c0c2ef63b57769b5e8def487
Zoe Lee <zoe@kihira.uk>
2017-11-08 10:30:11 +0100

    Add in all files from other repo

---
+++ swrpg-site.py
@@ -0,0 +1,111 @@
+from pymongo import MongoClient
+from flask import Flask, render_template, Markup, request, redirect, url_for
+from bson.objectid import ObjectId
+import os
+import jinja2
+
+app = Flask(__name__)
+app.jinja_options = {
+    "extensions": ['jinja2.ext.autoescape', 'jinja2.ext.with_'],
+    "autoescape": False
+}
+client = MongoClient(os.environ['DB_CONN'])
+db = client.starwars
+
+
+@app.route("/")
+def main_page():
+    return render_template("table.html", title="Hello", header=["First", "Second"],
+                           entries=[["1", "2"]])
+
+
+@app.route("/skills/")
+def skills():
+    entries = []
+    for skill in db.skills.find({}):
+        entry = [Markup("<a href=\"./{0}\">{0}</a>".format(skill["_id"])),
+                 skill["characteristic"], skill["type"], skill["index"]]
+        entries.append(entry)
+
+    return render_template("table.html", title="Skills", header=["Skill", "Characteristic", "Type", "Index"],
+                           entries=entries)
+
+
+@app.route("/gear/")
+def gear():
+    entries = []
+    for gear in db.gear.find({}):
+        entry = [Markup("<a href=\"./{0}\">{1}</a>".format(gear["_id"], gear["name"])),
+                 gear["price"], gear["encumbrance"], gear["rarity"], gear["index"]]
+        entries.append(entry)
+
+    return render_template("table.html", title="Items", header=["Item", "Price", "Encumbrance", "Rarity", "Index"],
+                           entries=entries)
+
+
+@app.route("/gear/<object_id>")
+def gear1(object_id):
+    gear = db.gear.find({"_id": ObjectId(object_id)})[0]
+    print(gear)
+
+    return render_template("item.html", title=gear["name"], item=gear)
+
+
+@app.route("/add-planet", methods=['GET', 'POST'])
+def add_planet():
+    if request.method == "POST":
+        planet = {
+            "_id": request.form["name"],
+            "astronavigation": {
+                "system": request.form["astronavigation"].split(",")[0][:-7].strip(),
+                "sector": request.form["astronavigation"].split(",")[1][:-7].strip(),
+                "region": request.form["astronavigation"].split(",")[2][:-7].strip()
+            },
+            "orbital": {
+                "days": int(request.form["orbital"].split("/")[0].strip().split(" ")[0].replace(",", "")),
+                "hours": int(request.form["orbital"].split("/")[1].strip().split(" ")[0].replace(",", ""))
+            },
+            "government": request.form["government"],
+            "population": request.form["population"],
+            "languages": strip_array(request.form["languages"].split(",")),
+            "terrain": request.form["terrain"],
+            "cities": {
+                "capital": request.form["capital"],
+                "others": strip_array(request.form["othercities"].split(","))
+            },
+            "areas": strip_array(request.form["areas"].split(",")),
+            "exports": strip_array(request.form["exports"].split(",")),
+            "imports": strip_array(request.form["imports"].split(",")),
+            "routes": strip_array(request.form["routes"].split(",")),
+            "conditions": request.form["conditions"],
+            "background": request.form["background"]
+        }
+        db.planets.insert(planet)
+        print(planet)
+        return redirect(url_for("add_planet"))
+    return render_template("add-planet.html")
+
+
+def strip_array(array):
+    for i in range(len(array)):
+        array[i] = array[i].strip()
+    return array
+
+
+symbols = {
+    "BOOST": "boost",
+    "SETBACK": "setback",
+    "ABILITY": "ability",
+    "PROFICIENCY": "proficiency",
+    "DIFFICULTY": "difficulty",
+    "CHALLENGE": "challenge"
+}
+
+
+def symbol(s):
+    for (key, value) in symbols.items():
+        s = s.replace("[{0}]".format(key), "<span class=\"symbol {0}\"></span>".format(value))
+    return s
+
+
+jinja2.filters.FILTERS["symbol"] = symbol
