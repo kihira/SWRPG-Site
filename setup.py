19e2c5024431d07aa33835ef97f0d0b3abb3bfb5
Zoe Lee <zoe@kihira.uk>
2017-11-13 17:12:53 +0100

    Add in scour and scripts to help with optimising svgs

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
 from setuptools import setup
+from os import listdir
+from distutils.cmd import Command
+
+
+class Options:
+    """Scour options"""
+    indent_type = 'none'
+    enable_viewboxing = True
+    strip_ids = True
+    strip_comments = True
+    shorten_ids = True
+
+
+class OptimiseSVG(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for file in listdir("assets"):
+            with open("assets/" + file, "rb") as input, open("server/static/img/" + file, "wb") as output:
+                from scour import scour
+                scour.start(Options, input, output)
+
 
 setup(
     name='swrpg-site',
@@ -7,4 +35,7 @@ setup(
     install_requires=[
         'flask', 'pymongo'
     ],
+    cmdclass={
+        'optimise': OptimiseSVG
+    }
 )
