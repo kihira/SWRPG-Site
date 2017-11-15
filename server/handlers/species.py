2e0185f9947ab01e290f1980c3542cc99f4305e5
Zoe <zoe.kihira.lee@gmail.com>
2017-11-15 17:39:06 +0100

    Add in species

---
+++ server/handlers/species.py
@@ -0,0 +1,24 @@
+from server.app import app
+from server.db import db
+from flask import Markup, render_template
+
+
+@app.route("/species/")
+def all_species():
+    import pymongo
+
+    entries = []
+    for species in db.species.find({}).sort("_id", pymongo.ASCENDING):
+        species["name"] = Markup(
+            "<a href=\"./{0}\">{1}</a>".format(species["_id"], species["_id"]).replace("_", " "))
+        entries.append(species)
+
+    return render_template("table.html", title="Abilities", header=["Ability", "Player"],
+                           fields=["name", "player"], entries=entries)
+
+
+@app.route("/species/<species_id>")
+def get_species(species_id):
+    species = db.species.find({"_id": species_id})[0]
+
+    return render_template("item.html", title=species["_id"].replace("_", " "), item=species)
