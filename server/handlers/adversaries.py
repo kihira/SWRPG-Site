d650ce889da4f6b44aca0514fe772768cece39d0
Zoe Lee <zoe@kihira.uk>
2018-02-12 16:25:37 +0100

    Initial work on improving adversaries

--- server/handlers/adversaries.py
+++ server/handlers/adversaries.py
@@ -22,9 +22,10 @@ def all_adversaries():
 
 
 @app.route("/adversaries/imperials")
+@app.route("/adversaries/imperial")
 def get_imperials():
     # todo this should probably use a tag system instead of regex search
-    return render_template("table.html", title="Adversaries",
+    return render_template("table.html", title="Adversaries - Imperials",
                            headers=["Type", "Skills", "Talents", "Abilities", "Equipment"],
                            fields=["level", "skills", "talents", "abilities", "equipment"],
                            entries=process_items(list(db.adversaries
@@ -33,9 +34,35 @@ def get_imperials():
                                                  .sort("name", pymongo.ASCENDING))))
 
 
+@app.route("/adversaries/alliance")
+@app.route("/adversaries/rebels")
+def get_rebels():
+    # todo this should probably use a tag system instead of regex search
+    return render_template("table.html", title="Adversaries - Alliance",
+                           headers=["Type", "Skills", "Talents", "Abilities", "Equipment"],
+                           fields=["level", "skills", "talents", "abilities", "equipment"],
+                           entries=process_items(list(db.adversaries
+                                                      .find({"$or": [{"name": {"$regex": "Rebel"}},
+                                                                     {"name": {"$regex": "Alliance"}},
+                                                                     {"tags": "rebel"}]})
+                                                      .sort("name", pymongo.ASCENDING))))
+
+
 @app.route("/adversaries/<object_id>")
 def get_adversary(object_id):
     from bson import ObjectId
     item = db.adversaries.find({"_id": ObjectId(object_id)})[0]
+    equipment = []
+    for i in item["equipment"]:
+        if "type" in i:
+            if i["type"] == "weapon":
+                equipment.append(db.weapons.find({"_id": i["id"]})[0]["name"])
+            elif i["type"] == "armor":
+                equipment.append(db.armor.find({"_id": i["id"]})[0]["name"])
+            elif i["type"] == "gear":
+                equipment.append(db.gear.find({"_id": i["id"]})[0]["name"])
+        else:
+            equipment.append(i)
+    item["equipment"] = equipment
 
     return render_template("adversary.html", title=item["name"], item=item)
