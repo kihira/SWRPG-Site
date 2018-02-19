a7ddeaa191b7d33111111e941612a96f226c768e
Zoe Lee <zoe@kihira.uk>
2018-02-19 11:45:26 +0100

    Add support for setting default order from url

--- server/static/js/table.ts
+++ server/static/js/table.ts
@@ -1,8 +1,30 @@
 import ColumnSettings = DataTables.ColumnSettings;
 
+interface Query {
+    search?: string;
+    asc?: number[];
+    desc?: number[];
+}
+
 function init(fields: string[], hasIndex: boolean, categories: boolean) {
-    const params: {[key: string]: string} = {};
-    location.search.substr(1).split("&").forEach((value) => { params[value.split("=")[0]] = value.split("=")[1]; });
+    const params: Query = {};
+    location.search.substr(1).split("&").forEach((value) => {
+        const data = value.split("=");
+        if (data[0] === "search") {
+            params.search = data[1];
+        }
+        switch (data[0]) {
+            case "search":
+                params.search = data[1];
+                break;
+            case "asc":
+                params.asc = data[1].split(/_/).map((value2) => parseInt(value2, 10));
+                break;
+            case "desc":
+                params.desc = data[1].split(/_/).map((value2) => parseInt(value2, 10));
+                break;
+        }
+    });
 
     const columns: ColumnSettings[] = [{data: "name"}];
     const settings: DataTables.Settings = {
@@ -25,17 +47,47 @@ function init(fields: string[], hasIndex: boolean, categories: boolean) {
 
     settings.columns = columns;
 
+    // Set default order based on query string
+    const order: Array<Array<(number | string)>> = [];
+    if (params.desc) {
+        params.desc.forEach((value) => {
+            order.push([value, "desc"]);
+        });
+    }
+    if (params.asc) {
+        params.asc.forEach((value) => {
+            order.push([value, "asc"]);
+        });
+    }
+    settings.order = order;
+
     const table = $("#data").DataTable(settings);
 
     const buildUrl = () => {
         const url = window.location.href.split("?")[0] + "?";
-        const newParams: any = {};
+        const newParams: { search?: string, asc?: string, desc?: string } = {};
+
+        const asc: number[] = [];
+        const desc: number[] = [];
         table.order().forEach((col) => {
-            newParams[col[0]] = col[1];
+            if (col[1] === "asc") {
+                asc.push(col[0] as number);
+            }
+            else if (col[1] === "desc") {
+                desc.push(col[0] as number);
+            }
         });
+        if (asc.length > 0) {
+            newParams.asc = asc.join("_");
+        }
+        if (desc.length > 0) {
+            newParams.desc = desc.join("_");
+        }
+
         if (table.search()) {
             newParams.search = table.search();
         }
+
         window.history.pushState(null, "", url + $.param(newParams));
     };
 
