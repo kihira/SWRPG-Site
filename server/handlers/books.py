1ec5a5821bd84a043e50eebfe3030f839fdf0a65
Zoe Lee <zoe@kihira.uk>
2018-04-06 16:16:56 +0200

    Switch over books to use the dynamic system

--- server/handlers/books.py
+++ server/handlers/books.py
@@ -1,19 +1,25 @@
 from pymongo.results import InsertOneResult, UpdateResult
 
+from model import Model, Field, TextareaField, SelectField
 from server.app import app
 from server.db import db
 from flask import render_template, abort, request
 
-
-def get_form_data():
-    return {
-        "_id": request.form.get("_id", ""),
-        "sku": request.form.get("sku", ""),
-        "isbn": request.form.get("isbn", ""),
-        "ffg_url": request.form.get("ffg_url", ""),
-        "release_date": request.form.get("release_date", ""),
-        "description": request.form.get("description", "")
-    }
+model = Model([
+    Field("_id", "Index"),
+    Field("name", "Name"),
+    SelectField("system", "System", [
+        "Edge of the Empire",
+        "Age of Rebellion",
+        "Force and Destiny",
+        "Star Wars Roleplaying"
+    ]),
+    Field("key", "SKU"),
+    Field("isbn", "ISBN"),
+    Field("ffg_url", "Product Page", html_type="url"),
+    Field("release_date", "Release Date", html_type="date"),
+    TextareaField("description", "Description")
+])
 
 
 @app.route("/books/")
@@ -36,10 +42,10 @@ def get_book(book_id):
 @app.route("/books/add", methods=['GET', 'POST'])
 def add_book():
     if request.method == "POST":
-        item = get_form_data()
+        item = model.from_form(request.form)
         result: InsertOneResult = db.books.insert_one(item)
-        return render_template("edit/book.html", item=item, added=True)
-    return render_template("edit/book.html")
+        return render_template("edit/add-item.html", item=item, model=model, added=True)
+    return render_template("edit/add-item.html", model=model)
 
 
 @app.route("/books/<object_id>/edit", methods=['GET', 'POST'])
@@ -50,9 +56,9 @@ def edit_book(object_id: str):
     item = item[0]
 
     if request.method == "POST":
-        new_item = get_form_data()
+        new_item = model.from_form(request.form)
         result: UpdateResult = db.books.update_one({"_id": item["_id"]}, {"$set": new_item})
-        return render_template("edit/book.html", title=item["name"], item=new_item,
+        return render_template("edit/add-item.html", title=item["name"], item=new_item, model=model,
                                updated=(result.modified_count == 1))
-    # return the template with values filled out if we haven't received any data
-    return render_template("edit/book.html", title=item["name"], item=item)
+
+    return render_template("edit/add-item.html", title=item["name"], item=item, model=model)
