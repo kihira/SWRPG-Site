d22e68b9e2bb245d46219a14844cc83d44978a28
Zoe Lee <zoe@kihira.uk>
2018-04-10 16:22:20 +0200

    Add custom watch script

---
+++ watch.js
@@ -0,0 +1,33 @@
+const watch = require("node-watch");
+const SVGO = require("svgo");
+const fs = require("fs");
+const path = require("path");
+const exec = require("child_process").exec;
+
+const svgo = new SVGO();
+
+watch('./assets/svg', {filter: /\.svg$/, recursive: true}, (evt, name) => {
+    fs.readFile(name, (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        svgo.optimize(data).then(result => {
+            if (result.error) {
+                console.error(result.error)
+            }
+            else {
+                const basename = path.basename(name);
+                fs.writeFile(path.resolve(__dirname, 'static', 'img', basename), result.data, (err, data) => {
+                    if (result.error) console.error(result.error);
+                    else console.log("Optimised " + basename)
+                });
+            }
+        })
+    });
+});
+
+watch('./assets/style.less', (evt, name) => {
+    const less = exec("npm run less");
+    less.stderr.pipe(process.stderr);
+    less.stdout.pipe(process.stdout);
+});
