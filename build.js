92a545a23a13127df3eaf6d556a1c1e622d04408
Zoe Lee <zoe@kihira.uk>
2018-04-18 17:59:46 +0200

    Start work on build.js

--- build.js
+++ build.js
@@ -1,5 +1,67 @@
 const fs = require("fs-extra");
+const path = require("path");
+const csso = require("csso");
+const SourceMapConsumer = require("source-map").SourceMapConsumer;
+const svgo = new require("svgo")();
 
-fs.copy("./assets/images", "./static/img/", (err) => {
-    if (err) console.log(err);
-});
+function buildImages() {
+    fs.ensureDirSync("./static/img");
+
+    // PNG images
+    fs.copy("./assets/images", "./static/img/", (err) => {
+        if (err) console.log(err);
+    });
+
+    // SVG
+    fs.readdir("./assets/svg", (err, files) => {
+        files.forEach((file) => {
+            const data = fs.readFileSync(file);
+            svgo.optimize(data)
+                .then(result => {
+                    if (result.error) {
+                        console.error(result.error);
+                    }
+                    else {
+                        writeFile("static/img", path.parse(name).name, result.data);
+                    }
+                })
+                .catch((err) => console.error(err.stack));
+        })
+    });
+}
+
+function minifyCss(data, filename, sourceMap) {
+    const result = csso.minify(data, {filename: filename, sourceMap: true, comments: false});
+    if (sourceMap) {
+        result.map.applySourceMap(new SourceMapConsumer(sourceMap), filename)
+    }
+    result.css += `\n/*# sourceMappingURL=${path.parse(filename).name}.css.map */`;
+    return result
+}
+
+function buildCss() {
+    fs.readFile(name, 'utf8', (err, data) => {
+        const pathData = path.parse(name);
+        less.render(data, {
+            sourceMap: {},
+            paths: [pathData.dir],
+            filename: name // need to pass full path to ensure it works with @import
+        })
+            .then((result) => {
+                result = minifyCss(result.css, pathData.base, result.map);
+                writeFile("static/css", `${pathData.name}.css`, result.css);
+                writeFile("static/css", `${pathData.name}.css.map`, result.map);
+                console.info(`Compiled ${pathData.base}`)
+            })
+            .catch((err) => {
+                console.error(err.stack);
+            })
+    });
+    fs.readFile(name, "utf8", (err, data) => {
+        const pathData = path.parse(name);
+        const result = minifyCss(data, name);
+        writeFile("static/css", `${pathData.name}.css`, result.css);
+        writeFile("static/css", `${pathData.name}.css.map`, result.map);
+        console.info(`Minified ${pathData.base}`)
+    });
+}
