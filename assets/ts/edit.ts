c4f7081ab9eb2ad54738279c67902918b8e255d2
Zoe <zoe.kihira.lee@gmail.com>
2019-03-05 20:36:03 +0000

    Work a little more on the editing side of things

--- assets/ts/edit.ts
+++ assets/ts/edit.ts
@@ -4,34 +4,33 @@ interface KeyDict {
 
 class RepeatableSection {
     private readonly container: JQuery;
-    private template: JQuery;
+    private readonly template: JQuery;
     private sectionsCount: number = 0;
 
-    constructor(fieldset: HTMLElement) {
-        this.template = $(fieldset).clone();
+    constructor(template: HTMLElement) {
+        this.template = $(template);
 
         const $addNew = $("<div>Add New</div>")
             .on("click", this.addNew);
         this.container = $("<div></div>", { class: "repeatable_container" })
+            .appendTo("#repeatables")
             .append($addNew);
-
-        $(fieldset)
-            .after(this.container)
-            .remove();
     }
 
     // todo add support for more nested documents (specifically if something has 2 of the same weapon)
     public addExisting(data: number[] | string[] | boolean[]) {
         for (const value of data) {
             const section = this.addNew();
+            const input = section.get()[0].querySelector("input") as HTMLInputElement;
+
             if (typeof value === "number") {
-                section.find(":input").val(value);
+                input.value = value.toString();
             }
             else if (typeof value === "string") {
-                section.find(":input").val(value);
+                input.value = value;
             }
             else if (typeof value === "boolean") {
-                section.find(":input").prop("checked", value);
+                input.checked = value;
             }
         }
     }
@@ -44,14 +43,16 @@ class RepeatableSection {
         this.sectionsCount += 1;
         return this.template
             .clone()
+            .appendTo(this.container)
+            .show()
             .find(":input")
-            .each((index: number, element: HTMLElement) => {
+            .each((index, element) => {
                 const newId = element.id + this.sectionsCount;
                 $(element).prev().attr("for", newId);
                 element.id = newId;
+                console.log("yay");
             })
-            .end()
-            .appendTo(this.container);
+            .end();
     }
 }
 
@@ -60,6 +61,6 @@ const repeatableSections: { [key: string]: RepeatableSection } = {};
 $(() => {
     $("fieldset")
         .each(function(this: HTMLElement) {
-            repeatableSections[this.id] = new RepeatableSection(this);
+            repeatableSections[this.id] = new RepeatableSection(this as HTMLElement);
         });
 });
